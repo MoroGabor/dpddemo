c44cc887c727e95af8773a2a72f6e2fcc1ef27ec
Moro Gabor <morogabor82@gmail.com>
2024-04-07 11:40:46 +0200

    Refactored docker

--- Dockerfile
+++ Dockerfile
@@ -1,25 +1,7 @@
-# Build Stage
-FROM amazoncorretto:17-alpine-jdk AS build
+FROM openjdk:17-jdk-slim
 
 WORKDIR /app
 
-COPY .mvn .mvn
-COPY mvnw .
-
-COPY pom.xml .
-COPY src src
-RUN chmod +x mvnw
-RUN dos2unix mvnw
-RUN ./mvnw clean package
-
-# Package Stage
-FROM amazoncorretto:17-alpine-jdk
-
-RUN adduser -g user -s /usr/bin/nologin --home /home/user user --disabled-password
-WORKDIR /home/user
-
-COPY --from=build /app/target/demo-0.0.1-SNAPSHOT.jar ./demo.jar
-
-EXPOSE 8080
+COPY target/demo-0.0.1-SNAPSHOT.jar ./demo.jar
 
 CMD ["java", "-jar", "demo.jar"]
--- RunDemo.bat
+++ RunDemo.bat
@@ -1,3 +1,21 @@
 @echo off
-echo Building and starting the application...
-docker-compose up --build
+
+rem Ellenőrizzük, hogy a Docker fut-e
+docker --version >nul 2>&1
+if %errorlevel% neq 0 (
+  echo Docker nincs telepítve vagy nem fut. Kérlek telepítsd és indítsd el a Docker-t.
+  exit /b 1
+)
+
+rem Ellenőrizzük, hogy a Docker Compose fut-e
+docker-compose --version >nul 2>&1
+if %errorlevel% neq 0 (
+  echo Docker Compose nincs telepítve. Kérlek telepítsd.
+  exit /b 1
+)
+
+rem Docker konténerek indítása
+echo Docker konténerek indítása...
+docker-compose up -d
+
+echo Docker konténerek sikeresen elindítva.
